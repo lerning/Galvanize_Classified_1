bcdca2db562e981840acc095c68310e5bc7da680
Joshua Lerner <joshualerner@Joshuas-MacBook-Pro.local>
2017-05-17 11:18:28 -0600

    get all and get single id both functional

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -1,10 +1,27 @@
-
 'use strict';
 
 const express = require('express');
-
 const router = express.Router();
+const knex = require('../knex');
 
-// YOUR CODE HERE
+
+router.get('/', (req, res) => {
+    knex('classifieds')
+      .select(['id','description', 'title', 'price', 'item_image'])
+      .then((classifieds) => {
+         res.send(classifieds)
+      })
+})
+
+router.get('/:id', (req, res) => {
+   let id  = req.params.id
+   knex('classifieds')
+      .where('id', id)
+      .select(['id','description', 'title', 'price', 'item_image'])
+      .first()
+      .then((classifieds) => {
+         res.send(classifieds)
+      })
+   })
 
 module.exports = router;
